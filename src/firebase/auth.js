5764f50380dbe4bce4583fd65843104864e6dc15
Christever <christian@steverlynck.fr>
2026-09-26 15:17:14 +0200

    refactor: suppression de la gestion des erreurs dans la fonction de connexion

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -3,17 +3,12 @@ import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "./config";
 
 export async function connexion(email, password) {
-  try {
+
   return await signInWithEmailAndPassword(
     auth,
     email,
     password
   );
-}
-catch (error){
-   console.error("Erreur de connexion :", error);
-    throw error;
-}
 }
 
 export async function deconnexion() {
